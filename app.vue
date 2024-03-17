68c7d9e93616db027194dce599ed70ea3e1f1ce1
samozamo <samozamo@hotmail.com>
2024-03-17 17:36:52 +0000

    add list of countries available as buttons

--- app.vue
+++ app.vue
@@ -1,5 +1,39 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <main>
+    <h1>News from around the world!</h1>
+    <ul>
+      <li v-for="country in countries" :key="country.code">
+        <button>
+          {{ country.label }}
+        </button>
+      </li>
+    </ul>
+  </main>
 </template>
+
+<script setup lang="ts">
+const countries = [
+  {
+    label: "US",
+    code: "us",
+  },
+  {
+    label: "UK",
+    code: "gb",
+  },
+  {
+    label: "France",
+    code: "fr",
+  },
+  {
+    label: "Australia",
+    code: "au",
+  },
+  {
+    label: "India",
+    code: "in",
+  },
+];
+</script>
+
+<style></style>
