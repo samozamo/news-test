a480438f4b402a5c4b21cadbe90e0a1ce9891799
samozamo <samozamo@hotmail.com>
2024-03-17 18:17:24 +0000

    add headlinesListItem component

---
+++ components/HeadlinesListItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <article class="headline">
+    <a :href="props.article.url">
+      <h4 class="headline__title">{{ props.article.title }}</h4>
+      <div class="headline__detail-group">
+        <p class="headline__author">Author: {{ props.article.author }}</p>
+        <p class="headline__source">Source: {{ props.article.source.name }}</p>
+      </div>
+    </a>
+  </article>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ article: Article }>();
+</script>
+
+<style scoped>
+a {
+  color: currentColor;
+  text-decoration: none;
+}
+
+a:hover .headline__title {
+  text-decoration: underline;
+}
+
+.headline {
+  line-height: 1.5;
+  padding: 0.5rem;
+}
+
+.headline__title {
+  font-size: 1.25rem;
+}
+
+.headline__detail-group {
+  font-size: 0.75rem;
+}
+</style>
